1bfd2d620326f901c15ab6455af1ac5881bfc8d5
Ali Waleed <ali@scale3labs.com>
2025-08-22 18:56:22 +0300

    test

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel 200"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
